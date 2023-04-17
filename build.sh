61fc1fc42c40b08229d67986c68cf66aedb17f05
Rhodri Lloyd-Evans <rhodri.lloyd-evans@outlook.com>
2023-04-17 23:06:31 +0100

    Add output directory to build script

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 7.0 -InstallDir ./dotnet7
 ./dotnet7/dotnet --version
-./dotnet7/dotnet run -c Release
+./dotnet7/dotnet run -c Release -o output
